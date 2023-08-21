304b35479893736508e414e8fc64d65074b6e13f
Mukesh <akshat.gupta@lpinfotech.com>
2023-08-21 18:45:47 +0530

    intit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace YourNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter first number");
+            Double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second number");
+            Double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Double sum = num1 + num2;
+            Console.WriteLine("Sum of two number is" + sum);
+
+        
+
+           
+
+
+        }
+    }
+}
